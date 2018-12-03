8981d0a17a93b7cbaedf719a2da49121b06f639d
pengzg <869279160@qq.com>
2018-12-03 17:20:33 +0800

    modify

--- three_react.js
+++ three_react.js
@@ -1,6 +1,16 @@
 
-var element = <h1>这是第三个文件<h1>;
+var element = <h1>这是第三个文件</h1>;
 ReactDOM.render(
     element,
     document.getElementById('three')
+);
+
+/* 数组 */
+var element2  = [
+    <h1>这是数组</h1>,<h2>测试</h2>
+];
+
+ReactDOM.render(
+    element2,
+    document.getElementById("arr")
 );
