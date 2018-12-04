080e742fdb99815d2a33496982c314b2c7adc268
pengzg <869279160@qq.com>
2018-12-04 17:27:15 +0800

    four

--- four_react.js
+++ four_react.js
@@ -1,6 +1,6 @@
-var element1 = <h1>组件</h1>;
 
-ReactDom.render(
+var element1 = <h1>组件</h1>;
+ReactDOM.render(
     element1,
-    document.getElementById("first")
+    document.getElementById('first')
 );
